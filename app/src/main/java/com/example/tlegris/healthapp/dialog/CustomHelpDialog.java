611497399a19822045fc51da9560ceea2e4911ba
NIJI\tlegris <thomas.legris@niji.fr>
2019-01-04 15:07:48 +0100

    add custom dialog for profile screen

---
+++ app/src/main/java/com/example/tlegris/healthapp/dialog/CustomHelpDialog.java
@@ -0,0 +1,34 @@
+package com.example.tlegris.healthapp.dialog;
+
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+
+import com.example.tlegris.healthapp.R;
+
+
+public class CustomHelpDialog extends Dialog {
+
+
+    public CustomHelpDialog(Activity a) {
+        super(a);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.dialog_help_activity);
+
+        findViewById(R.id.dialog_help_activity_close_textview).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dismiss();
+            }
+        });
+    }
+}
